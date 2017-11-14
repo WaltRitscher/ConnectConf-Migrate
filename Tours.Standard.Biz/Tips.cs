a574ae4056b1edf3ef46ae82c2ffbcfe450f8a8b
Walt Ritscher <waltritscher@outlook.com>
2017-11-13 19:06:40 -0800

    Finished version of class migration

---
+++ Tours.Standard.Biz/Tips.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tours.NET.DAL.ToursDataSetTableAdapters;
+using static Tours.NET.DAL.ToursDataSet;
+
+namespace Tours.NET.Biz
+{
+  public class Tips
+  {
+
+    private static Tips current;
+
+    public static Tips Current => current ?? (current = new Tips());
+    private TravelTipsTableAdapter tipsTableAdapter;
+
+    protected TravelTipsTableAdapter ToursTableAdapter {
+      get { return tipsTableAdapter ?? (tipsTableAdapter = new TravelTipsTableAdapter()); }
+    }
+
+
+    public TravelTipsDataTable GetTips()
+    {
+      return ToursTableAdapter.GetAllTravelTips();
+    }
+  }
+}
---
+++ Tours.Standard.DAL/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tours.NET.DAL.Properties
+{
+  public partial class Settings
+  {
+    public class Default
+    {
+        public static string ToursConnectionString = @"Data Source=(localDB)\mssqllocaldb;AttachDbFilename=|DataDirectory|\Tours.mdf;Initial Catalog=Tours;Integrated Security=True";
+    }
+  }
+}
